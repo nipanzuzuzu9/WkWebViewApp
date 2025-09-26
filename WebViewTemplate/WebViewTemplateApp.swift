a3e0853bfcb462742d70a902bac1952977cd466f
nipanzuzuzu9 <nipanzuzuzu9@gmail.com>
2025-09-26 05:17:06 -0700

    Add files via upload

---
+++ WebViewTemplate/WebViewTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewTemplateApp.swift
+//  WebViewTemplate
+//
+//  Created by Adrian Tschanz on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
