13111c3a61c2f94d0cdd1e90f63d50bc27df264c
nipanzuzuzu9 <nipanzuzuzu9@gmail.com>
2025-09-27 03:04:09 -0700

    Delete WebViewTemplate directory

--- WebViewTemplate/WebResources/Notifications.js
+++
@@ -1,7 +0,0 @@
-function scheduleNotification(title, body, secondsFromNow) {
-   window.webkit.messageHandlers.scheduleNotification.postMessage({
-       title: title,
-       body: body,
-       time: secondsFromNow
-   });
-}
